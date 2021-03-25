22c41fbcede9b98598c97ec557004b7a9970c424
cernax <andyps3man@gmail.com>
2021-03-24 22:05:12 -0300

    test
    test

--- servicios varios/servicios-varios/src/App.js
+++ servicios varios/servicios-varios/src/App.js
@@ -1,9 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Image from 'react-bootstrap/Image'
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link href={'/Users/Users.js'}>Usurio</Nav.Link>
+              <Nav.Link href=''>Servicio</Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link href="#deets">
+                <Container>
+                <Row>
+                  <Col xs={6} md={4}>
+                    <Image width={'50em'} src="https://mdbootstrap.com/img/Photos/Avatars/avatar-5.jpg" roundedCircle />
+                  </Col>
+                </Row>
+                </Container>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </Router>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ servicios varios/servicios-varios/src/Users/Users.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Users extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>Please choose a repository from the list below.</p>
+            </div>
+        );
+    }
+}
+
